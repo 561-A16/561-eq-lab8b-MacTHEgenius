c4008a8febf757833112fd569bdafa77c13a5be2
MacTHEgenius <levesque.benoit@me.com>
2016-10-28 16:06:29 -0400

    The return of the bug 3

--- scripts/test-and-publish.sh
+++ scripts/test-and-publish.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Restore appweb and appweb.unittests packages
-dotnet restore
-dotnet test appweb.unittests
+#dotnet restore
+#dotnet test appweb.unittests
 
 # Publish app
 dotnet publish appweb -o /root/publish
