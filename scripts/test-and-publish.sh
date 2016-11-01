2918a1d2b718f8eedca37962adf25f667a46086b
MacTHEgenius <levesque.benoit@me.com>
2016-11-01 13:15:48 -0400

    Applying some changes

--- scripts/test-and-publish.sh
+++ scripts/test-and-publish.sh
@@ -1,10 +1,13 @@
 #!bin/bash
 
-set -e
+set -ev
 
 # Restore appweb and appweb.unittests packages
 dotnet restore
 dotnet test appweb.unittests
 
+# Remove old publish directory
+rm -rf $HOME/publish/*
+
 # Publish app
-dotnet publish appweb -o /root/publish
+dotnet publish appweb -o /root/publish -c release
