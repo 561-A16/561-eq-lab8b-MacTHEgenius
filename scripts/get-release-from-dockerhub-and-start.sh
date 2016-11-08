c6580f9552b0385d4d00a835d96fbf7f008380a5
MacTHEgenius <levesque.benoit@me.com>
2016-11-08 17:52:18 -0500

    Fixing issues on docker run command

--- scripts/get-release-from-dockerhub-and-start.sh
+++ scripts/get-release-from-dockerhub-and-start.sh
@@ -7,4 +7,4 @@ docker pull macthegenius/webapp:release
 # Stop and remove container based from macthegenius/webapp:release image
 docker rm $(docker stop -t 1 $(docker ps -q --filter="ancestor=macthegenius/webapp:release"))
 
-docker run -itd -p 6500:5000
+docker run -itd -p 6500:5000 macthegenius/webapp:release
